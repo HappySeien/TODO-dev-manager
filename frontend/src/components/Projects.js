03f04a02ff9ae8e17074a487f0832092efbfb554
Vladislav Goryakin <Happy.Seien@yandex.ru>
2022-09-12 22:33:12 +0300

    stage-11 #7
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- frontend/src/components/Projects.js
+++ frontend/src/components/Projects.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { MDBTable, MDBTableHead, MDBTableBody, MDBIcon, MDBBtn } from 'mdb-react-ui-kit';
+import { MDBTable, MDBTableHead, MDBTableBody, MDBIcon, MDBBtn, MDBInput } from 'mdb-react-ui-kit';
 
 
 const ProjectApi = 'http://localhost:8000/api/projects/'
@@ -31,9 +31,19 @@ const ProjectItem = ({ project, deleteProject }) => {
 }
 
 const ProjectList = ({ projects, deleteProject }) => {
+    const [value, setValue] = useState('')
+    const filtredProject = projects.filter(project => {
+        return project.name.toLowerCase().includes(value.toLowerCase())
+    })
     return (
     <>
-        <h1 className='text-center'>Projects</h1>
+            <h1 className='text-center'>Projects</h1>
+            <div className='d-flex mx-auto container-md'>
+                <form>
+                    <span>Search</span>
+                    <MDBInput label='Project name' id='search' name='search' type='text' onChange={(event) => setValue(event.target.value)} />
+                </form>
+            </div>
         <MDBTable className='container-md'>
             <MDBTableHead>
                 <tr>
@@ -45,7 +55,7 @@ const ProjectList = ({ projects, deleteProject }) => {
                 </tr>
             </MDBTableHead>
             <MDBTableBody>
-                    {projects.map((project) => <ProjectItem key={project.id.toString()} project={project} deleteProject={deleteProject}/>)}
+                    {filtredProject.map((project) => <ProjectItem key={project.id.toString()} project={project} deleteProject={deleteProject}/>)}
             </MDBTableBody>
         </MDBTable>
     </>
